161e0bb760f5b5cda1545329108d1da11d4ae1df
Noran Salah <noransalah840@gmail.com>
2024-12-24 01:53:11 +0200

    Q1

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            //1-Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+
+            #region 1-Passing By Value
+
+            //static void Swap( int X,int y )
+            //{ 
+            //    int temp = X;
+            //    X = y;
+            //    y = temp;
+            //    Console.WriteLine($"X= {X} , Y={y}");
+
+
+            //}
+
+
+            #endregion
         }
     }
 }

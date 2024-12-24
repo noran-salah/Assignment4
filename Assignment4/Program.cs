a1a6cc90110f11e35955e7c30c39b4d5ff57c748
Noran Salah <noransalah840@gmail.com>
2024-12-24 02:41:15 +0200

    Q15

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -465,6 +465,55 @@
 
 
 
+            //15 -.Consider an Array of Integer values with size N, having values as in this Example
+            //7   0    0   0  0  5   6   7   5   0   7   5   3
+
+            //write a program find the longest distance between Two equal cells.In this example.The distance is measured by the number Of cells- for example, the distance between the first and the fourth cell is 2(cell 2 and cell 3).
+
+            //In the example above, the longest distance is between the first 7 and the
+            //10th 7, with a distance of 8 cells, i.e.the number of cells between the 1st
+            //And the 10th 7s.
+
+            //Note:
+            //-Array values will be taken from the user
+            //-If you have input like 1111111 then the distance is the number of
+            //Cells between the first and the last cell.
+
+            //int Size;
+            //bool flage;
+            //do
+            //{
+            //    Console.WriteLine("Enter number of Size");
+            //    flage = int.TryParse(Console.ReadLine(), out Size);
+            //} while (!flage);
+
+            //int[] arr = new int[Size];
+
+            //for (int i = 0; i < Size; i++)
+            //{
+            //    bool flage02;
+            //    do
+            //    {
+            //        Console.WriteLine($"Enter number {i + 1}");
+            //        flage02 = int.TryParse(Console.ReadLine(), out arr[i]);
+            //    } while (!flage02);
+
+            //}
+            //int distance = 0;
+
+
+            //for (int i = 0; i < Size; i++)
+            //{
+            //    for (int j = i + 1; j < Size; j++)//[1,2,4,2,1,2,1] i=0,j=6
+            //    {
+            //        if (arr[i] == arr[j])
+            //            if (distance < j - i - 1) //3
+            //                distance = j - i - 1;//5
+
+            //    }
+            //}
+            //Console.WriteLine($"Distance = {distance}");
+
 
 
 
